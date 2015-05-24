0362114c4ccb778e92b4a2fad6858f1e2cf6fefa
PlasmaK <plasmak@gmail.com>
2015-05-24 06:22:46 -0400

    updated with assignment function

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,32 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## makes a cached matrix using these trickery of scoping rules!
 
 makeCacheMatrix <- function(x = matrix()) {
-
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverted) m <<- inverted
+  getinverse <- function() m
+  list(set = set, get = get, setinverse = setinverse, getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
+## takes an argument of an odd matrix saved as a form of a list from previous function, 
+## and checks if old caches matrix of same type exists.
+## if not, decided to calculate from scratch.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  m <- x$getinverse()
+  
+  if(!is.null(m)) {
+    message("getting cached data")
+    return(m)
+  }
+  
+  data <- x$get()
+  m <- solve(data, ...)
+  x$setinverse(m)
+  m
 }
